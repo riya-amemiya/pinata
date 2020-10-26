f671094977b9297eb8f48a94b566161e3691f956
Riya <hiyoriorria@gmail.com>
2020-10-26 22:18:24 +0900

    commit-auto

--- dist/index.js
+++ dist/index.js
@@ -1,38 +1,21 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-
-var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
-
-var _react = require('react');
-
-var _react2 = _interopRequireDefault(_react);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-exports.default = function (props) {
-    var _useState = (0, _react.useState)(0),
-        _useState2 = _slicedToArray(_useState, 2),
-        num = _useState2[0],
-        setNum = _useState2[1];
-
-    var text = null;
-    return _react2.default.createElement(
+import React, { useState } from 'react';
+export default (props => {
+    const [num, setNum] = useState(0);
+    let text = null;
+    return React.createElement(
         'div',
         null,
-        _react2.default.createElement(
+        React.createElement(
             'h1',
-            { onClick: function onClick() {
-                    console.log(num);
+            { onClick: () => {
+                    setNum(num + 1);
                 } },
             'Hello World'
         ),
-        _react2.default.createElement(
+        React.createElement(
             'p',
             null,
             text ? num : text + num
         )
     );
-};
+});
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 export default props =>
 {
     const [num, setNum] = useState(0)
     let text =null
     return (
         <div>
-            <h1 onClick={() => { console.log(num) }}>Hello World</h1>
+            <h1 onClick={() => { setNum(num + 1) }}>Hello World</h1>
             <p>{text ? num : text + num}</p>
         </div>
     )
